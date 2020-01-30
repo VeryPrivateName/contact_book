7cb21a2bd14e7d07f51ea63ee02d66c52af4956b
Me The Admin <webcontentyo@gmail.com>
2020-01-30 21:33:05 +0100

    Animations/Transitions

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
@@ -13,17 +14,39 @@ const Contacts = () => {
 
   return (
     <Fragment>
-      {filtered !== null // if filtered is Not null, map what is in it through contacts
-        ? //if
-          filtered.map(contact => (
-            <ContactItem key={contact.id} contact={contact} /> //  for each filtered contact load/show filtered contact
-          ))
-        : //else
-          contacts.map(contact => (
-            <ContactItem key={contact.id} contact={contact} /> // if filtered Is null, show contacts
-          ))}
+      <TransitionGroup>
+        {filtered !== null
+          ? // if filtered is Not null, map what is in it through contacts
+            filtered.map((
+              contact //  for each filtered contact load/show filtered contact
+            ) => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : //else filtered Is null, show contacts
+            contacts.map(contact => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
 
 export default Contacts;
+
+// Default:
+
+// <Fragment>
+//   {filtered !== null // if filtered is Not null, map what is in it through contacts
+//   ? //if
+//     filtered.map(contact => (
+//       <ContactItem key={contact.id} contact={contact} /> //  for each filtered contact load/show filtered contact
+//     ))
+//   : //else
+//     contacts.map(contact => (
+//       <ContactItem key={contact.id} contact={contact} /> // if filtered Is null, show contacts
+//     ))}
+// </Fragment>
