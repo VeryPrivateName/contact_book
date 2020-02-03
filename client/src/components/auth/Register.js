59ca04f5b37b1f7c243b3e2bde9a38f24161dc1c
Me The Admin <webcontentyo@gmail.com>
2020-02-03 19:25:14 +0100

    load user

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,19 +2,23 @@ import React, { useState, useContext, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
 import AuthContext from '../../context/auth/authContext';
 
-const Register = () => {
+const Register = props => {
   const alertContext = useContext(AlertContext);
   const authContext = useContext(AuthContext);
 
   const { setAlert } = alertContext;
-  const { register, error, clearErrors } = authContext;
+  const { register, error, clearErrors, isAuthenticated } = authContext;
 
   useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/'); //redirecting to home page
+    }
+
     if (error === 'User already exists') {
       setAlert(error, 'danger');
       clearErrors();
     }
-  }, [setAlert, clearErrors, error]); // adding values as a dependency to useEffect
+  }, [setAlert, clearErrors, error, isAuthenticated, props.history]); // adding values as a dependency to useEffect
 
   const [user, setUser] = useState({
     name: '',
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken';
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -27,8 +28,17 @@ const AuthState = props => {
 
   // Load User
 
-  const loadUser = () => {
-    console.log('loadUser');
+  const loadUser = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    try {
+      const res = await axios.get('/api/auth'); // checks the token if valid user
+
+      dispatch({ type: USER_LOADED, payload: res.data });
+    } catch (err) {
+      dispatch({ type: AUTH_ERROR });
+    }
   };
 
   // Register User
